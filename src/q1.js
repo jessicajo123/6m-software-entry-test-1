4d910f2f57c18578dfd9437f2715c5fb4d263c4c
jessicajo123 <130422508+jessicajo123@users.noreply.github.com>
2023-08-22 11:25:10 +0800

    Update q1.js
    Jessica Update q1.js

--- src/q1.js
+++ src/q1.js
@@ -11,8 +11,20 @@
 
 function swap(x, y){
     // Task 1: Add code here
+     if (typeof x !== 'number' || typeof y !== 'number') {
+        return -1;
+}
+     // Task 1: Swap the values of x and y
+    x = x + y;
+    y = x - y;
+    x = x - y;
+
+    // Print swapped values
+    console.log("Swapped values: x =", x, "y =", y);
 }
 
+
 // Task 2: Add code here
+swap(5, 10);
 
 module.exports = swap;
